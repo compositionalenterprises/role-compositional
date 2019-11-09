57e4707c6510f5f4d06e086b4821ece90f58faf4
AndrewCz <andrewcz@neomailbox.net>
2019-11-08 22:43:57 -0500

    Add new jekyll stuff

---
+++ files/jekyll_entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+#
+# This file provides the first setup and running of the server in Jekyll, based on a couple
+# environment variables:
+#
+#       - JEKYLL_PORT
+#       - JEKYLL_BASE_URL
+#       - JEKYLL_DOMAIN
+#
+
+jekyll_port="${JEKYLL_PORT:-8080}"
+
+git init
+git remote add origin https://gitlab.com/smacz/docker-andrewcz-blog.git
+git fetch
+git reset --hard origin/master
+
+# Test for the base URL existing and not being a blank string
+if [[ ! -z "${JEKYLL_BASE_URL}" ]]; then
+        # Add leading and trailing slashes as necessary
+        if [[ ${JEKYLL_BASE_URL:0:1} != '/' ]]; then
+                JEKYLL_BASE_URL="/${JEKYLL_BASE_URL}"
+        fi
+        if [[ ${JEKYLL_BASE_URL: -1} != '/' ]]; then
+                JEKYLL_BASE_URL="${JEKYLL_BASE_URL}/"
+        fi
+        sed -i "s#baseurl:.*#baseurl:        '$JEKYLL_BASE_URL'#" _config.yml
+fi
+
+# Same with the domain name
+if [[ ! -z "${JEKYLL_DOMAIN}" ]]; then
+        sed -i "s#url:.*#url:            '$JEKYLL_DOMAIN'#" _config.yml
+fi
+
+git clone git clone https://gitlab.com/smacz/posts.git _posts
+wget https://andrewcz.com/nextcloud/s/jg4kJePQsbbRZxb/download -O images.tar.gz \
+tar -xvzf images.tar.gz \
+rm -rf images.tar.gz
+mkdir _site
+jekyll serve -H 0.0.0.0 -P $jekyll_port
