adcb29da71e055bf8bf52bc268f081683b51a145
AndrewCz <andrewcz@neomailbox.net>
2020-04-12 21:56:37 -0400

    Remember colon...

--- bin/create_environment.py
+++ bin/create_environment.py
@@ -318,7 +318,7 @@ def main():
         create_vaulted_passwords(local_repo, service, vault_pass)
 
     # Add the mysql and portal passwords too, because we need those always
-    for default_service in ['mysql', 'portal']
+    for default_service in ['mysql', 'portal']:
         create_vaulted_passwords(local_repo, default_service, vault_pass)
 
     put_repo_in_gitlab(local_repo, args['domain'])
